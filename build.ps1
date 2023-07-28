18dc76abe32f090cc36980404f27f824b105292d
mmx <mmx003@gmail.com>
2023-07-28 12:12:00 +0300

    migrate to netcore 6 build IdentityServer4.csproj

--- build.ps1
+++ build.ps1
@@ -8,18 +8,18 @@ pushd ./src/Storage
 Invoke-Expression "./build.ps1 $args"
 popd
 
-pushd ./src/IdentityServer4
-Invoke-Expression "./build.ps1 $args"
-popd
+# pushd ./src/IdentityServer4
+# Invoke-Expression "./build.ps1 $args"
+# popd
 
-pushd ./src/EntityFramework.Storage
-Invoke-Expression "./build.ps1 $args"
-popd
+# pushd ./src/EntityFramework.Storage
+# Invoke-Expression "./build.ps1 $args"
+# popd
 
-pushd ./src/EntityFramework
-Invoke-Expression "./build.ps1 $args"
-popd
+# pushd ./src/EntityFramework
+# Invoke-Expression "./build.ps1 $args"
+# popd
 
-pushd ./src/AspNetIdentity
-Invoke-Expression "./build.ps1 $args"
-popd
+# pushd ./src/AspNetIdentity
+# Invoke-Expression "./build.ps1 $args"
+# popd
---
+++ src/migration-readme.txt
@@ -0,0 +1,5 @@
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+	
+	<PackageReference Include="IdentityModel" Version="6.1.0" />
+    <PackageReference Include="Microsoft.SourceLink.GitHub" PrivateAssets="All" Version="1.1.1" />
+    <PackageReference Include="Newtonsoft.Json" Version="13.0.3" />
