5e1ddd1fb7242ff60e86b43c6ed1ba621e31645a
George B <113766655+georgeberbat@users.noreply.github.com>
2023-08-21 16:32:26 +0300

    Net5 (#2)
    * AT-247 feat: update to net5.0
    
    * AT-247 fix: fix dependencies in IdentityServer4.csproj
    
    ---------
    
    Co-authored-by: Бербат Георгий <g.berbat@dextechnology.com>

--- src/IdentityServer4/test/IdentityServer.IntegrationTests/Common/JsonExtensions.cs
+++ src/IdentityServer4/test/IdentityServer.IntegrationTests/Common/JsonExtensions.cs
@@ -2,71 +2,72 @@
 using System.Collections.Generic;
 using System.Text.Json;
 
-namespace IdentityServer.IntegrationTests.Common;
-
-public static class JsonExtensions
+namespace IdentityServer.IntegrationTests.Common
 {
-    public static Dictionary<string, object> ToDictionary(this JsonElement element)
+    public static class JsonExtensions
     {
-        var dictionary = new Dictionary<string, object>();
-
-        foreach (var property in element.EnumerateObject())
+        public static Dictionary<string, object> ToDictionary(this JsonElement element)
         {
-            object value = property.Value.ValueKind switch
+            var dictionary = new Dictionary<string, object>();
+
+            foreach (var property in element.EnumerateObject())
             {
-                JsonValueKind.Object => ToDictionary(property.Value),
-                JsonValueKind.Array => ParseJsonArray(property.Value.EnumerateArray()),
-                JsonValueKind.String => property.Value.GetString(),
-                JsonValueKind.Number => property.Value.TryGetInt64(out var intValue)
-                    ? intValue
-                    : (object) property.Value.GetDecimal(),
-                JsonValueKind.True => true,
-                JsonValueKind.False => false,
-                JsonValueKind.Null => null,
-                _ => throw new ArgumentOutOfRangeException()
-            };
+                object value = property.Value.ValueKind switch
+                {
+                    JsonValueKind.Object => ToDictionary(property.Value),
+                    JsonValueKind.Array => ParseJsonArray(property.Value.EnumerateArray()),
+                    JsonValueKind.String => property.Value.GetString(),
+                    JsonValueKind.Number => property.Value.TryGetInt64(out var intValue)
+                        ? intValue
+                        : (object) property.Value.GetDecimal(),
+                    JsonValueKind.True => true,
+                    JsonValueKind.False => false,
+                    JsonValueKind.Null => null,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
 
-            dictionary.Add(property.Name, value);
-        }
-
-        return dictionary;
-    }
-
-    static List<object> ParseJsonArray(JsonElement.ArrayEnumerator arrayEnumerator)
-    {
-        var list = new List<object>();
-
-        foreach (var item in arrayEnumerator)
-        {
-            switch (item.ValueKind)
-            {
-                case JsonValueKind.Object:
-                    list.Add(ToDictionary(item));
-                    break;
-                case JsonValueKind.Array:
-                    list.Add(ParseJsonArray(item.EnumerateArray()));
-                    break;
-                case JsonValueKind.String:
-                    list.Add(item.GetString());
-                    break;
-                case JsonValueKind.Number:
-                    list.Add(item.TryGetInt32(out int intValue) ? intValue : (object) item.GetDecimal());
-                    break;
-                case JsonValueKind.True:
-                    list.Add(true);
-                    break;
-                case JsonValueKind.False:
-                    list.Add(false);
-                    break;
-                case JsonValueKind.Null:
-                    list.Add(null);
-                    break;
-                case JsonValueKind.Undefined:
-                default:
-                    throw new ArgumentOutOfRangeException();
+                dictionary.Add(property.Name, value);
             }
+
+            return dictionary;
         }
 
-        return list;
+        static List<object> ParseJsonArray(JsonElement.ArrayEnumerator arrayEnumerator)
+        {
+            var list = new List<object>();
+
+            foreach (var item in arrayEnumerator)
+            {
+                switch (item.ValueKind)
+                {
+                    case JsonValueKind.Object:
+                        list.Add(ToDictionary(item));
+                        break;
+                    case JsonValueKind.Array:
+                        list.Add(ParseJsonArray(item.EnumerateArray()));
+                        break;
+                    case JsonValueKind.String:
+                        list.Add(item.GetString());
+                        break;
+                    case JsonValueKind.Number:
+                        list.Add(item.TryGetInt32(out int intValue) ? intValue : (object) item.GetDecimal());
+                        break;
+                    case JsonValueKind.True:
+                        list.Add(true);
+                        break;
+                    case JsonValueKind.False:
+                        list.Add(false);
+                        break;
+                    case JsonValueKind.Null:
+                        list.Add(null);
+                        break;
+                    case JsonValueKind.Undefined:
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            return list;
+        }
     }
 }
